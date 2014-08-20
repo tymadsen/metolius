1bb11b85ec360ae618890376131abac5a69212c2
nemophrost <madsenat@gmail.com>
2014-08-20 13:31:35 -0600

    changed to loupe instead of jqzoom

--- js/main.js
+++ js/main.js
@@ -8,33 +8,55 @@ function MM_jumpMenuGo(objId, targ, restore) { //v9.0
 }
 
 $(function() {
+  // menu toggling
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active')
   });
 
+  // menubar images (Spry)
   var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {
     imgRight: "SpryAssets/SpryMenuBarRightHover.gif"
   });
-  
-  $('.zoom-gallery-preview').jqzoom({
-    zoomType: 'innerzoom',
-    lens: true,
-    title: false,
-    preloadImages: false,
-    alwaysOn: false,
-    zoomWidth: 300,
-    zoomHeight: 250,
-    showEffect: 'fadein',
-    hideEffect: 'fadeout'
-    // xOffset: 90,
-    // yOffset: 30,
-    // position:'left' 
-  });
 
-  $('.zoom-gallery-preview').on('click', '.zoomPup', function() {
-    $.fancybox([{
-      href: $(this).closest('.zoomPad').find('.zoomWrapperImage img').attr('src')
-    }]);
+  // product thumbnails/gallery
+  var previewContainer = $('.zoom-gallery-preview'),
+      thumbContainer = $('#product-image-thumbs-hrz'),
+      largeImages = [],
+      selectedImage = 0;
+
+  thumbContainer.find('a[data-img-sm]').each(function(i) {
+    var smImg = $(this).data('img-sm'),
+        lgImg = $(this).data('img-lg');
+
+    largeImages.push({
+      href: lgImg
+    });
+
+    $(this).click(function(e) {
+      e.preventDefault();
+
+      if ($(this).hasClass('selected')) {
+        return;
+      }
+
+      thumbContainer.find('a.selected').removeClass('selected');
+      $(this).addClass('selected');
+
+      selectedImage = i;
+
+      previewContainer.find('img').prop('src', smImg);
+      $('.loupe').hide().find('img').prop('src', lgImg);
+    });
+  }).first().click();
+
+  previewContainer.loupe({
+    width: 240,
+    height: 240
+  }).add('.loupe').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    $.fancybox(largeImages, { index: selectedImage });
   });
 
   $('.fancybox-media').fancybox({
